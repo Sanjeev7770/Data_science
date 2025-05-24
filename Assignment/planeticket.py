bc927dc4d75cc4b75d2f0ca374a72a5041675530
sanjeev <birajparajuli81@gmail.com>
2025-05-24 12:29:45 +0545

    assignment completed

---
+++ Assignment/planeticket.py
@@ -0,0 +1,22 @@
+distance=float(input('Distance you are about to travel in Kilometers'))
+clas=float(input('Which class would you prefer'))
+Fair=0
+
+if clas==1:
+    fair=distance*5
+    print(fair)
+
+elif clas==2:
+    fair=distance*3
+    print(fair)
+
+elif clas==3:
+    fair=distance*1
+    print(fair)
+
+else:
+    distance<=500
+    discount=fair*0.1
+
+    
+    
